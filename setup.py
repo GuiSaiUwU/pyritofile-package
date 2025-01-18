0214f4a9c31751fc8242f863f972d9e47e14779c
GuiSaiUwU <107002177+GuiSaiUwU@users.noreply.github.com>
2025-01-17 22:59:36 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+# Silly commands:
+# python setup.py bdist bdist_wheel
+# twine upload dist/*
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setup(
+    name="pyritofile",
+    version="0.2.0",
+    
+    author="GuiSaiUwU",
+    author_email="contaguardian@gmail.com",
+    url="https://github.com/GuiSaiUwU/pyritofile-package",
+    description="Python package to deal with League of Legends files.",
+    
+    packages=find_packages(),
+    install_requires=[
+        "xxhash>=3.5.0",
+        "pyzstd>=0.16.2"
+    ],
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    
+    license="GPLv3+"
+)
---
+++ tests/uwu.py
@@ -0,0 +1,50 @@
+from loguru import logger
+from pyritofile import SKN, SKL, BIN
+from requests import get
+
+
+def test_bin():
+    logger.info("Testing BIN")
+    response = get("https://raw.communitydragon.org/latest/game/globals.cdtb.bin")
+    
+    bin_file = BIN()
+    bin_file.read('', raw=response.content)
+    if (bin_file.write('', True) == response.content):
+        logger.info("BIN Test passed")
+    else:
+        logger.error("BIN Test failed")
+
+
+def test_skn():
+    logger.info("Testing SKN")
+    response = get("https://raw.communitydragon.org/latest/game/assets/characters/akali/skins/base/akali.skn")
+    
+    skn_file = SKN()
+    skn_file.read('', raw=response.content)
+
+    if (skn_file.write('', True) + b"\x00"*12 == response.content):
+        logger.info("SKN Test passed")
+    else:
+        logger.error("SKN Test failed")
+
+
+def test_skl():
+    """
+    Disabled bcs is always failing
+    """
+    logger.info("Testing SKL")
+    response = get("https://github.com/GuiSaiUwU/EveryRiotSKLFile/raw/refs/heads/main/skl/assets/characters/ahri/skins/base/ahri_base.skl")
+    
+    skl_file = SKL()
+    skl_file.read('', raw=response.content)
+    skl_file.write('a.skl')
+    if (skl_file.write('', True) == response.content):
+        logger.info("SKL Test passed")
+    else:
+        logger.error("SKL Test failed")
+
+
+if __name__ == '__main__':
+    test_bin()
+    test_skn()
+    #test_skl()
